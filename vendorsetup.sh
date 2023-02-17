f72ec9e44cc957049cccd98ce5281faff6dd073d
akmacc <arumugamguna7058@gmail.com>
2023-02-17 07:13:42 +0000

    add vendorsetup
    Signed-off-by: akmacc <arumugamguna7058@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+# Vendor Tree
+git clone https://github.com/akmacc/vendor_realme_x3.git -b 13 vendor/realme/x3
+
+# Kernel Tree
+git clone https://github.com/akmacc/kernel_realme_x3.git -b msm-4.14 kernel/realme/x3
+
+# Clang
+git clone --depth=1 https://gitlab.com/varunhardgamer/trb_clang.git -b 17 prebuilts/clang/host/linux-x86/trb_clang
+
+# Other
+git clone https://github.com/Realme-X3-Series/hardware_oplus.git hardware/oplus
